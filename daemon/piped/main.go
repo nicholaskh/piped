4c1f1d534cb30dc4df2ceb0e1a3e3cbac502e84c
Nicholas Zhang <kaihong.zhang@funplusgame.com>
2015-05-21 17:14:44 +0800

    lockfile and kill operation

--- daemon/piped/main.go
+++ daemon/piped/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"time"
 
+	"github.com/nicholaskh/golib/locking"
 	"github.com/nicholaskh/golib/server"
 	"github.com/nicholaskh/piped/config"
 	"github.com/nicholaskh/piped/engine"
@@ -15,7 +18,24 @@ func init() {
 		server.ShowVersionAndExit()
 	}
 
+	if options.kill {
+		if err := server.KillProcess(options.lockFile); err != nil {
+			fmt.Fprintf(os.Stderr, "stop failed: %s\n", err)
+		}
+
+		os.Exit(0)
+	}
+
 	server.SetupLogging(options.logFile, options.logLevel, options.crashLogFile)
+
+	if options.lockFile != "" {
+		if locking.InstanceLocked(options.lockFile) {
+			fmt.Fprintf(os.Stderr, "Another piped is running, exit...\n")
+			os.Exit(1)
+		}
+
+		locking.LockInstance(options.lockFile)
+	}
 
 	conf := server.LoadConfig(options.configFile)
 	config.PipedConf = new(config.PipedConfig)
